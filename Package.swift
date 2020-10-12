1ed0077fc9e6d387ff8971b5c90f46855fce9245
Hal Lee <hal@lee.me>
2020-10-12 09:14:59 -0400

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "MerchantKit",
+    defaultLocalization: "en",
+    platforms: [.iOS(.v11), .macOS(.v10_12), .tvOS(.v11)],
+    products: [
+        .library(name: "MerchantKit", targets: ["MerchantKit"])
+    ],
+    targets: [
+        .target(name: "MerchantKit", path: "Source"),
+        .testTarget(
+            name: "MerchantKitTests",
+            dependencies: ["MerchantKit"],
+            path: "Tests",
+            resources: [.copy("Sample Resources")]
+        )
+    ]
+)
